6b091ce6fb9639ac3e1fd4d25216aaae3798a89a
Radhakkrishnan <radhakkrishnan9.2@gmail.com>
2025-05-03 08:33:07 +0530

    Added vercel.json to fix 404 routing issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
-  rewrites: [
-      { "source": "/(.*)", "destination": "/" }
-    ]
-  
-  
- 
 })
