75933c6b4dfe259093692b7be63ac750bef01fd8
Radhakkrishnan <radhakkrishnan9.2@gmail.com>
2025-05-03 08:24:44 +0530

    deployment version

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  
+  rewrites: [
+      { "source": "/(.*)", "destination": "/" }
+    ]
+  
+  
  
 })
